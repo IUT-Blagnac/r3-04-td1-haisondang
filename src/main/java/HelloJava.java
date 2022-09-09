4c4e6092ddba020185882904eaad13cd3e2283d4
haisondang <100203120+haisondang@users.noreply.github.com>
2022-09-09 18:00:57 +0200

    Add files via upload

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -2,6 +2,7 @@
  * Ceci est une classe de test qui ne fait pas grand chose
  * @author JMB
  * @author LC
+ * modifié par Hai Son DANG, BUT 2
  */
 public class HelloJava {
 	public static void main(String[] args) {
